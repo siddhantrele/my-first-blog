1dc89be678d1457516a1ae6f150ca86a5b0c2cb4
Siddhant Rele <Siddhant Rele>
2016-04-26 10:18:01 +0530

    Safe commit

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from .forms import PostForm
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
@@ -15,7 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required	
+@login_required
 def post_new(request):
      if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,7 +30,7 @@ def post_new(request):
          form = PostForm()
      return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required	 
+@login_required
 def post_edit(request, pk):
      post = get_object_or_404(Post, pk=pk)
      if request.method == "POST":
@@ -45,7 +45,7 @@ def post_edit(request, pk):
          form = PostForm(instance=post)
      return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required	 
+@login_required
 def post_draft_list(request):
      posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
      return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -61,7 +61,7 @@ def post_remove(request, pk):
      post = get_object_or_404(Post, pk=pk)
      post.delete()
      return redirect('blog.views.post_list')
-	 
+
 def add_comment_to_post(request, pk):
      post = get_object_or_404(Post, pk=pk)
      if request.method == "POST":
@@ -74,7 +74,7 @@ def add_comment_to_post(request, pk):
      else:
          form = CommentForm()
      return render(request, 'blog/add_comment_to_post.html', {'form': form})
-	 
+
 @login_required
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ import django.contrib.auth.views
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
+	url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')), 
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^accounts/login/$', django.contrib.auth.views.login),
 	url(r'^accounts/logout/$', django.contrib.auth.views.logout, {'next_page': '/'}),
